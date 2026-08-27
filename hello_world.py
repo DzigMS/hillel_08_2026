13486570f33cfb84c43ff7dff0ca99dd80ff46d3
DzMS <dzigmihail@gmail.com>
2026-08-27 20:20:48 +0300

    Add hello_world file

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+print('Hello world')
+
+for i in range(10):
+    print(i)
+
+
+print('end program')
+
+
+first_number: int = int(10)
+print(type(int))
+print(first_number)
+
+first_number = 20
+print(first_number)
+
+# first_number = 'Hello wold2'
+# print(first_number)
+
+# + - * /
+# ==
+
+second_number = 7
+# 7 + 10
+result = first_number + second_number
+
+print(result)
